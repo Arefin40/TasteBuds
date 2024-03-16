9f6bd0bc93dbb2371081641e9fbb7eec9a3c5fb6
ShahriarArefin <arfin17310@gmail.com>
2024-03-16 20:49:23 +0600

    exported all icons

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+export { default as Account } from "./Account";
+export { default as Search } from "./Search";
+export { default as Calory } from "./Calory";
+export { default as Menu } from "./Menu";
+export { default as Time } from "./Time";
+
+export { default as Facebook } from "./Facebook";
+export { default as Twitter } from "./Twitter";
+export { default as Instagram } from "./Instagram";
+export { default as Github } from "./Github";
