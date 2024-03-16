e7af3a7ba9a68b1883a40b8391caee71ca259b94
ShahriarArefin <arfin17310@gmail.com>
2024-03-17 01:12:12 +0600

    updated tailwindcss config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,10 +6,10 @@ export default {
          fontFamily: {
             sans: ["Mulish", "sans-serif"],
          },
-         backgroundImage: (theme) => ({
+         backgroundImage: {
             banner:
-               "linear-gradient(to bottom, #150B2BBF, #150B2B33), url('Banner.jpg')",
-         }),
+               "linear-gradient(to bottom, #150B2BBF, #150B2B33), url('./src/assets/Banner.jpg')",
+         },
          gridTemplateColumns: {
             recipe: "48rem 1fr",
             preparing: "0.75rem 1fr repeat(2,0.5fr) 5.25rem",
@@ -17,7 +17,7 @@ export default {
             "hero-buttons": "repeat(auto-fit, minmax(12.125rem,1fr))",
          },
          listStyleImage: {
-            recipe: 'url("Bullet.png")',
+            recipe: 'url("./src/assets/Bullet.png")',
          },
       },
    },
